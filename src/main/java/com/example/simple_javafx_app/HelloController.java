7e867752d5f729ce4d76fab33dec6d27d031002e
ChaosPainter <liszkawojciech92@gmail.com>
2023-03-12 17:28:50 +0100

    commit #2 - change label text color implemented

--- src/main/java/com/example/simple_javafx_app/HelloController.java
+++ src/main/java/com/example/simple_javafx_app/HelloController.java
@@ -2,13 +2,24 @@ package com.example.simple_javafx_app;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label cameleon;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    @FXML private void on_click_niebieski()
+    {
+        cameleon.setTextFill(Color.BLUE);
+    }
+
+    @FXML private void on_click_zieleony()
+    {
+        cameleon.setTextFill(Color.GREEN);
+    }
+
+    @FXML private void on_click_czerwony()
+    {
+        cameleon.setTextFill(Color.RED);
     }
 }
